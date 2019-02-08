750fb1a147cc8a4e59c644ee9af0830f92de3a29
s.bitomsky <stephan.bitomsky@easy.de>
2019-02-08 17:29:55 +0300

    ;add comments to properties

--- generator/template/cfw.config.js
+++ generator/template/cfw.config.js
@@ -1,7 +1,27 @@
 module.exports = {
+  
+  /**
+   * hostname of your backend system
+   */
   host: '',
+  
+  /**
+   * customer name of the account
+   */
   customer: '',
+  
+  /**
+   * user or email address of a registered user
+   */
   user: '',
+  
+  /**
+   * password of the registered user
+   */
   password: '',
+  
+  /**
+   * selected backend application
+   */
   app: '',
 };
